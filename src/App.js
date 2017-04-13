fc1dc8551958aadbcec64d4a57037577a6f5cdbb
Lusan <daslusan@lifcare.in>
2017-04-13 15:51:54 +0530

    nested components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: 'this is the state txt'
-    }
-  }
-
-  update(e) {
-    this.setState({
-      txt: e.target.value
-    })
-  }
-
   render() {
     return (
-      <div>
-        <p>{this.state.txt} {this.props.cat}</p>
-        <Widget update={this.update.bind(this)} />
-      </div>
+      <Button> <Heart />React </Button>
     )
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update} />
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
+}
+const Button = (props) =>
+  <button>{props.children}</button>
 
 App.propTypes = {
   txt: React.PropTypes.string,
