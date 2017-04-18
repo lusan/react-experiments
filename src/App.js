2cf64faef8961ba3b01f96aa493c86fbf5a38f5f
Lusan <daslusan@lifcare.in>
2017-04-18 19:09:00 +0530

    playing with component life cycle

--- src/App.js
+++ src/App.js
@@ -10,17 +10,20 @@ class App extends React.Component {
     this.setState({val: this.state.val + 1 })
   }
   componentWillMount(){
-    console.log('mounting')
+    console.log('mounting');
+    this.setState({m: 2});
   }
   render(){
     console.log('rendering!')
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>{this.state.val * this.state.m}</button>
   }
   componentDidMount(){
-    console.log('mounted')
+    console.log('mounted');
+    this.inc = setInterval(this.update, 500);
   }
   componentWillUnmount(){
-    console.log('bye!')
+    console.log('bye!');
+    clearInterval(this.inc);
   }
 }
 
